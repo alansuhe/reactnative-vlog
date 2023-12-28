c14cc615eea247ed53c74c33947ce629b70f8fff
alansuhe <tosuhe@gmail.com>
2023-12-28 10:10:23 +0800

    实战优化

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,51 +9,53 @@ import React from 'react';
 import {
   Pressable,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import { useStyle } from '../style'
+import { useStyle, cm, bcm, mcm, scm } from '../style'
 
 function App(): React.JSX.Element {
 
-  const { s, sc } = useStyle()
+  const { s, sc, Colors: { emphasis } } = useStyle()
 
   return (
     <SafeAreaView style={[s.container, s.centered]}>
 
-      <View style={sc.card}>
+      <View style={[sc.card, s.shadow]}>
         <Text style={s.titleText}>Home</Text>
         <Text style={s.subTitleText}>Sub title</Text>
 
         <Text style={s.normalText}>normal blah .....</Text>
         <Text style={s.subText}>sub text</Text>
 
-        <View style={s.row}>
-          <Pressable style={sc.buttonAct}>
+        <View style={[s.row, { marginBottom: bcm * 2}]}>
+          <Pressable style={sc.boxAct}>
             <Text style={s.normalText}>act</Text>
           </Pressable>
-          <Pressable style={sc.buttonLink}>
+          <Pressable style={sc.boxLink}>
             <Text style={s.normalText}>link</Text>
           </Pressable>
         </View>
       </View>
 
-      <View style={s.row}>
-        <Pressable style={sc.buttonAct}>
+      <>
+        <Pressable style={sc.boxAct}>
           <Text style={s.normalText}>act</Text>
         </Pressable>
-        <Pressable style={sc.buttonLink}>
+        <Pressable style={sc.boxLink}>
           <Text style={s.normalText}>link</Text>
         </Pressable>
+        <View style={s.round}>
+          <Text>R</Text>
+        </View>
+        <View style={sc.roundAct}>
+          <Text>A</Text>
+        </View>
 
-        <Text style={s.normalText}>normal blah .....</Text>
+        <Text style={s.normalText}>normal blah .....<Text style={{ color: emphasis }}>Emphasis highLight</Text></Text>
         <Text style={s.subText}>sub text</Text>
-      </View>
+      </>
 
     </SafeAreaView>
   );
